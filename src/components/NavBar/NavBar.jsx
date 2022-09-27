b8bf15587edcefe51a1d2a9159f624f48310328b
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-09-27 16:05:04 -0300

    add bornoz tickets logo to title and header

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,8 +4,11 @@ import CartWidget from '../CartWidget/CartWidget';
 const NavBar = () => {
   return (
     <div>
-        <div className='flex justify-between px-16 pb-6 mt-6'>
+        <div className='flex justify-between px-16 pb-6 mt-6 items-center'>
+          <div className='flex items-center gap-6'>
+            <img src='src/assets/bornozTickets.png' className='w-16'/>
             <h1 className="font-semibold">Bornoz Tickets</h1>
+          </div>
             <ul className='flex gap-4 items-center'>
                 <li><a href="#">Inicio</a></li>
                 <li><a href="#">Eventos</a></li>
