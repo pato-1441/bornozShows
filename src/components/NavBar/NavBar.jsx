6ad10e480af7cd5dfbc57a0b948f979554274230
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-10-02 21:20:36 -0300

    fixing rooting of assets, and bornozTicketsLogo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import CartWidget from '../CartWidget/CartWidget';
+import React from "react";
+import CartWidget from "../CartWidget/CartWidget";
+import bornozTicketsLogo from "/assets/bornozTickets.png";
 
 const NavBar = () => {
   return (
     <div>
-        <div className='flex justify-between px-16 pb-6 mt-6 items-center'>
-          <div className='flex items-center gap-6'>
-            <img src='src/assets/bornozTickets.png' className='w-16'/>
-            <h1 className="font-semibold">Bornoz Tickets</h1>
-          </div>
-            <ul className='flex gap-4 items-center'>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Eventos</a></li>
-                <li><a href="#">Contacto</a></li>
-                <CartWidget/>
-            </ul>
-        </div>    
+      <div className="mt-6 flex items-center justify-between px-16 pb-6">
+        <div className="flex items-center gap-6">
+          <img src={bornozTicketsLogo} className="w-16" />
+          <h1 className="font-semibold">Bornoz Tickets</h1>
+        </div>
+        <ul className="flex items-center gap-4">
+          <li>
+            <a href="#">Inicio</a>
+          </li>
+          <li>
+            <a href="#">Eventos</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+          <CartWidget />
+        </ul>
+      </div>
     </div>
   );
 };
