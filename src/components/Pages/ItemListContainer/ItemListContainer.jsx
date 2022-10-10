84d9b00d15c1231f0854460557a91bf3facf9b04
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-10-10 20:19:03 -0300

    acomodo paddings, estios

--- src/components/Pages/ItemListContainer/ItemListContainer.jsx
+++ src/components/Pages/ItemListContainer/ItemListContainer.jsx
@@ -16,36 +16,36 @@ const ItemListContainer = ({greeting}) => {
   console.log(productos)
   return (
       <>
-          <div className='text-center text-3xl pt-10'>{greeting}</div>
-          <div className="grid grid-cols-3">
-          { loading ?
-              <h2>Cargando...</h2>
-                :
-              productos.map(prod=>  <div key={prod.id} >
-                                      <div className="card card-compact w-48 bg-base-100 shadow-xl">
-                                      <figure><img src={prod.image}/></figure>
-                                      <div className="card-body">
-                                        <h2 className="card-title">{prod.name}</h2>
-                                        <p>{prod.price}</p>
-                                        <div className="card-actions justify-end">
-                                          <button className="btn btn-primary">Comprar</button>
+          <h1 className='text-center text-3xl py-4'>{greeting}</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+            { loading ?
+                <button className="btn loading bg-inherit border-none">loading</button>
+                  :
+                productos.map(prod=>  <div key={prod.id} >
+                                        <div className="card card-compact w-64 bg-base-100 shadow-xl">
+                                        <figure><img src={prod.image}/></figure>
+                                        <div className="card-body">
+                                          <h2 className="card-title">{prod.name}</h2>
+                                          <p>{prod.price}</p>
+                                          <div className="card-actions justify-end">
+                                            <button className="btn btn-primary">Comprar</button>
+                                          </div>
                                         </div>
                                       </div>
-                                    </div>
-                                    </div>,
-                                    
-                                    {/* <div key={prod.id} className="card card-compact w-96 bg-base-100 shadow-xl">
-                                      <figure><img src={prod.image}/></figure>
-                                      <div className="card-body">
-                                        <h2 className="card-title">{prod.name}</h2>
-                                        <p>{prod.price}</p>
-                                        <div className="card-actions justify-end">
-                                          <button className="btn btn-primary">Buy Now</button>
+                                      </div>,
+                                      
+                                      {/* <div key={prod.id} className="card card-compact w-96 bg-base-100 shadow-xl">
+                                        <figure><img src={prod.image}/></figure>
+                                        <div className="card-body">
+                                          <h2 className="card-title">{prod.name}</h2>
+                                          <p>{prod.price}</p>
+                                          <div className="card-actions justify-end">
+                                            <button className="btn btn-primary">Buy Now</button>
+                                          </div>
                                         </div>
-                                      </div>
-                                    </div> */}
-                            )
-          }
+                                      </div> */}
+                              )
+            }
           </div>
       </>
 
