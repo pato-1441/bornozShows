afaa4a9ce90f63ed5171451facf3a52b7732ca0b
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-10-10 17:23:07 -0300

    add Pages y ItemDetailContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import NavBar from "./components/NavBar/NavBar";
+import Navbar from "./components/Navbar/Navbar";
+import ItemListContainer from "./components/Pages/ItemListContainer/ItemListContainer"
+import ItemDetailContainer from "./components/Pages/ItemDetailContainer/ItemDetailContainer";
 
 function App() {
 
   return (
     <div className="container-fluid">
       <div className="h-screen bg-gradient-to-b from-gray-900 to-slate-800 flex flex-col text-white">
-        <NavBar />
-        <ItemListContainer greeting={'Bienvenido a mi web'}/>
+        <Navbar />
+        <ItemListContainer greeting={'Bienvenido a mi web'} />
+        <ItemDetailContainer />        
       </div>
     </div>
   );
