86452632b8ac7eab65b5687d5be514c616c22e76
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-10-10 17:13:38 -0300

    agregando daisyUI

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 const CartWidget = () => {
   return (
-    <li>
-        <a href="#">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
-        <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
-        </svg>
-        </a>
-    </li>
-  );
+        <div className="flex-none">
+          <a href="#">
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                  <span className="badge badge-sm indicator-item">0</span>
+                </div>
+              </label>
+            </div>
+          </a>
+        </div>
+    );
 };
 
 export default CartWidget;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,23 +5,18 @@ import bornozTicketsLogo from "/assets/bornozTickets.png";
 const NavBar = () => {
   return (
     <div>
-      <div className="mt-6 flex items-center justify-between px-16 pb-6">
-        <div className="flex items-center gap-6">
-          <img src={bornozTicketsLogo} className="w-16" />
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl"><img src={bornozTicketsLogo} className="w-16"/></a>
           <h1 className="font-semibold">Bornoz Tickets</h1>
         </div>
-        <ul className="flex items-center gap-4">
-          <li>
-            <a href="#">Inicio</a>
-          </li>
-          <li>
-            <a href="#">Eventos</a>
-          </li>
-          <li>
-            <a href="#">Contacto</a>
-          </li>
-          <CartWidget />
-        </ul>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0">
+            <li><a>Inicio</a></li>
+            <li><a>Eventos</a></li>
+          </ul>
+        </div>
+        <CartWidget />
       </div>
     </div>
   );
