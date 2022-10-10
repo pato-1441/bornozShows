6c9fa7ce3db3b35afb0e5eed72555ac43d6585ed
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-10-10 17:57:03 -0300

    metiendo router-dom y aplicando routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import Navbar from "./components/Navbar/Navbar";
 import ItemListContainer from "./components/Pages/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/Pages/ItemDetailContainer/ItemDetailContainer";
+import CarritoPage from "./components/Pages/CarritoPage/CarritoPage";
 
 function App() {
 
   return (
-    <div className="container-fluid">
-      <div className="h-screen bg-gradient-to-b from-gray-900 to-slate-800 flex flex-col text-white">
-        <Navbar />
-        <ItemListContainer greeting={'Bienvenido a mi web'} />
-        <ItemDetailContainer />        
-      </div>
-    </div>
+    <BrowserRouter>
+      <div className="container-fluid">
+        <div className="h-screen bg-gradient-to-b from-gray-900 to-slate-800 flex flex-col text-white">
+          <Navbar />
+
+          <Routes>
+            <Route path='/' element={<ItemListContainer greeting={'Bienvenido a mi web'} />} />
+            <Route path='/detail' element={<ItemDetailContainer /> } />
+            <Route path='/cart' element={<CarritoPage />} />           
+                   
+          </Routes>
+          
+        </div>
+      </div>    
+    </BrowserRouter>
   );
 }
 
